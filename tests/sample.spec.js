417a939add39f36c67deb6e1b82ec3590ad57a0d
prasadmudedla <prasadmudedla@live.in>
2019-11-01 14:19:43 +0530

    first commit

---
+++ tests/sample.spec.js
@@ -0,0 +1,20 @@
+describe('sample test', function () {
+    let page;
+
+    before(async function () {
+        page = await browser.newPage();
+        await page.setDefaultNavigationTimeout(10000);
+        await page.goto(params.baseUrl, { waitUntil: 'domcontentloaded'});
+        console.log(params);
+    });
+
+    after(async function () {
+        await page.close();
+    })
+
+    it('should have the correct page title', async function () {
+        expect(await page.title()).to.eql('The Internet');
+        expect(await page.$eval('.heading', el => el.textContent)).to.eql('Welcome to the-internet');
+    });
+
+});
---
+++ util/bootstrap.js
@@ -0,0 +1,60 @@
+const puppeteer = require('puppeteer');
+const puppeteer_ff = require('puppeteer-firefox');
+const { expect } = require('chai');
+const config = require('config');
+const minimist = require('minimist');
+const _ = require('lodash');
+const globalVariables = _.pick(global, ['browser', 'expect', 'params']);
+
+// puppeteer options
+const ff_opts = {
+    headless: false,
+    defaultViewport: null,
+    args: ['--start-fullscreen'],
+    executablePath: '/Applications/Firefox.app/Contents/MacOS/firefox'
+};
+
+const chrome_opts = {
+    headless: false,
+    defaultViewport: null,
+    args: ['--start-fullscreen'],
+    executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+}
+
+const edge_opts = {
+    headless: false,
+    defaultViewport: null,
+    args: ['--start-fullscreen'],
+    executablePath: '/Applications/Microsoft Edge Beta.app/Contents/MacOS/Microsoft Edge Beta'
+}
+
+// expose variables
+before(async function () {
+
+    // assigning the assertions globally
+    global.expect = expect;
+
+    // fetching the command line arguments
+    var args = minimist(process.argv.slice(2));
+
+    // assiging  the environment globally
+    const env = args.env;
+    global.params = config.get(env);
+
+    // crearting the browser as per the command line arguments
+    if(args.br==='chrome') {
+        global.browser = await puppeteer.launch(chrome_opts);
+    } else if(args.br ==='firefox') {
+        global.browser = await puppeteer_ff.launch(ff_opts);
+    } else if(args.br = 'edge') {
+        global.browser = await puppeteer.launch(edge_opts);
+    }
+});
+
+// close browser and reset global variables
+after(async function () {
+    await browser.close();
+    global.browser = globalVariables.browser;
+    global.expect = globalVariables.expect;
+    global.params = globalVariables.params;
+});
