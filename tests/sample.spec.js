c16e105b9d75bbc0c0b79dcebf64d39da36dab84
prasadmudedla <prasadmudedla@live.in>
2019-11-02 18:53:28 +0530

    Refactor: replace function with arrow functions in spec

--- tests/sample.spec.js
+++ tests/sample.spec.js
@@ -1,17 +1,17 @@
-describe('sample test', function () {
+describe('sample test', () => {
     let page;
 
-    before(async function () {
+    before(async () => {
         page = await browser.newPage();
         await page.setDefaultNavigationTimeout(10000);
         await page.goto(params.baseUrl, { waitUntil: 'domcontentloaded'});
     });
 
-    after(async function () {
+    after(async () => {
         await page.close();
     })
 
-    it('should have the correct page title', async function () {
+    it('should have the correct page title', async ()  => {
         expect(await page.title()).to.eql('The Internet');
         expect(await page.$eval('.heading', el => el.textContent)).to.eql('Welcome to the-internet');
     });
