a495dda683823ad47895132c630020894c573775
prasadmudedla <prasadmudedla@live.in>
2019-11-02 18:48:39 +0530

    Add chromium as the default browser

--- util/bootstrap.js
+++ util/bootstrap.js
@@ -4,48 +4,49 @@ const { expect } = require('chai');
 const config = require('config');
 const minimist = require('minimist');
 
-// puppeteer options
-const ff_opts = {
+
+const defaultOptions = {
     headless: false,
     defaultViewport: null,
-    args: ['--start-fullscreen'],
+    args: ['--start-fullscreen']
+}
+// puppeteer options
+const ff_opts = {
+    ...defaultOptions,
     executablePath: '/Applications/Firefox.app/Contents/MacOS/firefox'
 };
 
 const chrome_opts = {
-    headless: false,
-    defaultViewport: null,
-    args: ['--start-fullscreen'],
+    ...defaultOptions,
     executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
 }
 
 const edge_opts = {
-    headless: false,
-    defaultViewport: null,
-    args: ['--start-fullscreen'],
+    ...defaultOptions,
     executablePath: '/Applications/Microsoft Edge Beta.app/Contents/MacOS/Microsoft Edge Beta'
 }
 
 // expose variables
 before(async function () {
-
     // assigning the assertions globally
     global.expect = expect;
 
     // fetching the command line arguments
-    var args = minimist(process.argv.slice(2));
+    const args = minimist(process.argv.slice(2));
 
     // assiging  the environment globally
     const env = args.env;
     global.params = config.get(env);
 
     // crearting the browser as per the command line arguments
-    if(args.br==='chrome') {
+    if(args.br === 'chrome') {
         global.browser = await puppeteer.launch(chrome_opts);
-    } else if(args.br ==='firefox') {
+    } else if(args.br === 'firefox') {
         global.browser = await puppeteer_ff.launch(ff_opts);
-    } else if(args.br = 'edge') {
+    } else if(args.br === 'edge') {
         global.browser = await puppeteer.launch(edge_opts);
+    } else {
+        global.browser = await puppeteer.launch(defaultOptions);
     }
 });
 
